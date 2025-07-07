a67cc4bada83e54d7fda9d326f77404fdd274241
Linus <linuswong9@gmail.com>
2025-07-07 01:37:43 -0700

    Added all assets and fixed Nativewind implentation

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import { Text, View } from "react-native";
-import "./global.css";
- 
-export default function App() {
+import './global.css';
+
+
+export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-5xl font-bold text-center text-primary font-quicksand-bold">
+        Welcome to my React Native App!
       </Text>
     </View>
   );
